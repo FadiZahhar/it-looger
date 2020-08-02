0e5119da4aba5639c7097a8dd1b37f634cb58536
fadizahhar <fadi.zahhar@ogilvy.com>
2020-08-02 15:56:18 +0300

    adding the reducer actions and types

---
+++ src/actions/logActions.js
@@ -0,0 +1,29 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+  /*return async (dispatch) => {
+
+  };*/
+};
+
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
